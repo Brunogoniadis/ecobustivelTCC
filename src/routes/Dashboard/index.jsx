8e5a0bb8dada83ade4f6ce07ab4aa5318ddeec94
brunogoniadis <brunogoniadis@gmail.com>
2023-03-13 21:20:52 -0300

    <Styled: topbar & slider created>

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -1,6 +1,35 @@
+import { MainWindow } from "./styles"
+import Slider from 'react-input-slider';
+import { useState } from "react";
+
 const Dashboard = () => {
+
+    const [state, setState] = useState({ x: 1});
+
+
     return (
-      <h1>Dashboard</h1>
+      <MainWindow>
+        <div className="Topbar">
+          <p>ecobustivelTCC</p>
+        </div>
+
+        <div className="TitleText">
+
+          <h2>Selecionar posto</h2>
+          <p>Os postos de gasolina seram mostrados conforme o limite escolhido</p>
+
+          <Slider 
+            styles={{
+              track:{backgroundColor: "var(--color-text)"},
+              active: {backgroundColor: 'var(--color-border)' },
+            }}
+          axis="x" x={state.x} onChange={({ x }) => setState(state => ({ ...state, x }))} xmax={10} />
+
+          <p>{state.x} km</p>
+        </div>
+
+        
+      </MainWindow>
     );
   };
   
---
+++ src/routes/Dashboard/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const MainWindow = styled.main`
+    height: 667px;
+    width: 375px;
+    background-color: var(--color-backgroundWindow);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+
+    gap: 35px;
+
+    color: black;
+
+    .Topbar{
+        color: white;
+        text-align: center;
+    
+    }
+
+    .TitleText{
+        text-align: center;
+        background-color: #fff;
+        width: 100%;
+        height: 20%;
+
+        padding: 5%;
+    }
+`
