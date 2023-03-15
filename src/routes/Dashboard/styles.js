0e092984425f5b8db7b1638b89be251bd9c7f46a
brunogoniadis <brunogoniadis@gmail.com>
2023-03-14 21:44:11 -0300

    <feat: stationlist>

--- src/routes/Dashboard/styles.js
+++ src/routes/Dashboard/styles.js
@@ -29,9 +29,23 @@ export const MainWindow = styled.main`
         padding: 5%;
     }
     .MapDiv{
-        height: 250px;
+        height: 260px;
         width: 100%;
-
-        background-color: red;
+    }
+    .TableInfo{
+        padding: 4%;
+        height: 15vh;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 40% 20% 20% 20%;
+        grid-template-rows: 33% 33% 33% ;
+        row-gap:1px;
+        vertical-align: middle;
+        p{
+            color: #fff;
+            font-weight: normal;
+            border: 1px solid black;
+            font-size: 12px;
+        }
     }
 `
