95cd8e0d047b35115440b197bd1349332d6cd42c
brunogoniadis <brunogoniadis@gmail.com>
2023-03-14 22:29:22 -0300

    <feat: map added in dashboard screen>

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -8,10 +8,17 @@ const Dashboard = () => {
   const [state, setState] = useState({ x: 1 });
 
   const [location, setLocation] = useState({ lat: null, lng: null });
+  const [gasStations, setGasStations] = useState([
+    { name: "Posto Planetário ", location: { lat: -22.8429, lng: -43.0744 }, gasolinaValue: 22.90, alcoolValue: 14.12, gnvValue: 5.43 },
+    { name: "Posto Barreira's ", location: { lat: -22.8382, lng: -43.0672 }, gasolinaValue: 21.67, alcoolValue: 13.23, gnvValue: 4.34 },
+    { name: "Posto Ipiranga ", location: { lat: -22.8351, lng: -43.0607 }, gasolinaValue: 20.76, alcoolValue: 13.45, gnvValue: 4.12 },
+  ]);
+
+
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "NULL"
+    googleMapsApiKey: "AIzaSyDATcqFrpKhpxJxugCvy0Q0c6R3PwZ43Io"
   });
 
 
@@ -46,7 +53,7 @@ const Dashboard = () => {
             track: { backgroundColor: "var(--color-text)" },
             active: { backgroundColor: 'var(--color-border)' },
           }}
-          axis="x" x={state.x} onChange={({ x }) => setState(state => ({ ...state, x }))} xmax={10} 
+          axis="x" x={state.x} onChange={({ x }) => setState(state => ({ ...state, x }))} xmax={10}
         />
 
         <p>{state.x} km</p>
@@ -56,14 +63,35 @@ const Dashboard = () => {
           {isLoaded ? (
             <GoogleMap
               mapContainerStyle={{ width: '100%', height: '100%' }}
-              center={{ lat: location.lat, lng: location.lng, }}
+              center={{ lat: location.lat, lng: location.lng }}
               options={options}
-              zoom={16}
+              zoom={13}
             >
-              <></>
+              {gasStations.map((gasStation, index) => (
+                <Marker
+                  key={index}
+                  position={gasStation.location}
+                />
+              ))}
             </GoogleMap>
           ) : <></>}
         </div>
+
+        {gasStations.map((gasStation, index) => (
+          <React.Fragment key={index}>
+            <Marker
+              position={gasStation.location}
+              title={gasStation.name}
+            />
+            <div className="TableInfo">
+              <p className="gasStation">{gasStation.name}</p>
+              <p className="gasolinaValue">{gasStation.gasolinaValue}</p>
+              <p className="alcoolValue">{gasStation.alcoolValue}</p>
+              <p className="gnvValue">{gasStation.gnvValue}</p>
+            </div>
+          </React.Fragment>
+        ))}
+
       </div>
     </MainWindow>
   );
